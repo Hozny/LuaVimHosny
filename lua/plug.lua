2e26a8537e6e4b8b35fe9eea974f5d4373f78b05
Yousof Hosny <yhosnysa@uwaterloo.ca>
2021-09-23 12:29:39 -0400

    Added auto installation of Packer if not found

--- lua/plug.lua
+++ lua/plug.lua
@@ -1,3 +1,14 @@
+local fn = vim.fn
+
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+
+if fn.empty(fn.glob(install_path)) > 0 then
+    execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
+    execute "packadd packer.nvim"
+end
+
+vim.cmd("autocmd BufWritePost plugins.lua PackerCompile") -- Auto compile when there are changes in plugins.lua
+
 return require('packer').startup(function(use)
 	-- packer self management
 	use('wbthomason/packer.nvim')
