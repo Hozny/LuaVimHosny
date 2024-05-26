c606135371483bd2121754ed945856d2cfa0073e
Yousof Hosny <yousof@Yousofs-MacBook-Pro-2.local>
2024-05-25 21:25:51 -0700

    kitt-navigator

--- lua/mystuff/lazy.lua
+++ lua/mystuff/lazy.lua
@@ -320,4 +320,19 @@ return require('lazy').setup({
     {'terrortylor/nvim-comment'}, -- Commenting plugin
 
     {'github/copilot.vim'},
+
+    -- kitty specific
+    {
+        "MunsMan/kitty-navigator.nvim",
+        keys = {
+            {"<C-h>", function()require("kitty-navigator").navigateLeft()end, desc = "Move left a Split", mode = {"n"}},
+            {"<C-j>", function()require("kitty-navigator").navigateDown()end, desc = "Move down a Split", mode = {"n"}},
+            {"<C-k>", function()require("kitty-navigator").navigateUp()end, desc = "Move up a Split", mode = {"n"}},
+            {"<C-l>", function()require("kitty-navigator").navigateRight()end, desc = "Move right a Split", mode = {"n"}},
+        },
+        build = {
+            "cp navigate_kitty.py ~/.config/kitty",
+            "cp pass_keys.py ~/.config/kitty",
+        }
+    }
 })
